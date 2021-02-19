f0a7b8f3260473a6ed1feedc7a1d59fa37c89af3
huangjia7 <zouyi202@163.com>
2021-02-19 16:01:51 +0800

    Update SpringBootMain.java

--- src/main/java/com/linkus/SpringBootMain.java
+++ src/main/java/com/linkus/SpringBootMain.java
@@ -7,9 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-//@EnableAutoConfiguration
 public class SpringBootMain {
-	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootMain.class, args);
 	}
